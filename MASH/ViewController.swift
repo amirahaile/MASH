d96c27a7d5f64aa77ca535833cc3bb7d744f131f
Kyna <khn710@gmail.com>
2015-11-09 11:27:38 -0800

    Reconfigured constraints on dot.

--- MASH/ViewController.swift
+++ MASH/ViewController.swift
@@ -20,27 +20,30 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var lInputThree: UITextField!
 
     @IBOutlet weak var lInputFour: UITextField!
-    
-    func dot() {
-        let dot : UIView = UIView()
-        dot.translatesAutoresizingMaskIntoConstraints = false
-        dot.backgroundColor = UIColor.blackColor()
-        self.view.addSubview(dot)
-        self.view.addConstraints( NSLayoutConstraint.constraintsWithVisualFormat("H:[dot(==50)]", options: .DirectionLeftToRight, metrics: nil, views: ["dot" : dot])
-        )
-        self.view.addConstraints( NSLayoutConstraint.constraintsWithVisualFormat("V:[dot(==50)]", options: .DirectionLeftToRight, metrics: nil, views: ["dot" : dot])
-        )
-        self.view.addConstraints( NSLayoutConstraint.constraintsWithVisualFormat("H:[lInputOne]-[dot(==50)]", options: .DirectionLeftToRight, metrics: nil, views: ["dot" : dot, "lInputOne" : lInputOne])
-        )
-        self.view.addConstraint( NSLayoutConstraint.init(item: dot, attribute: .CenterY, relatedBy: .Equal, toItem: lInputOne, attribute: .CenterY, multiplier: 1, constant: 0))
-        
 
+    let dot : UIView = UIView()
+
+    
+    func configDot() {
+        self.dot.translatesAutoresizingMaskIntoConstraints = false
+        self.dot.backgroundColor = UIColor.blackColor()
+        self.view.addSubview(dot)
+        self.view.addConstraints( NSLayoutConstraint.constraintsWithVisualFormat("V:[dot(==25)]", options: .DirectionLeftToRight, metrics: nil, views: ["dot" : self.dot])
+        )
+        self.view.addConstraints( NSLayoutConstraint.constraintsWithVisualFormat("H:[lInputOne]-[dot(==25)]", options: .DirectionLeftToRight, metrics: nil, views: ["dot" : self.dot, "lInputOne" : self.lInputOne])
+        )
+        self.view.addConstraint( NSLayoutConstraint.init(item: dot, attribute: .CenterY, relatedBy: .Equal, toItem: self.lInputOne, attribute: .CenterY, multiplier: 1, constant: 0))
+    
+    }
+    
+    override func viewDidLayoutSubviews() {
+        self.dot.layer.cornerRadius = self.dot.bounds.width / 2
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.lInputOne.delegate = self
-        dot()
+        configDot()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
