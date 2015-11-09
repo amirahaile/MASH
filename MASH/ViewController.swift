85e102d13a030dc19c88222386318443e0f1a889
Kyna <khn710@gmail.com>
2015-11-09 09:15:30 -0800

    "playing with oauth"

--- MASH/ViewController.swift
+++ MASH/ViewController.swift
@@ -9,7 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+//      WHERE DOES THIS GO?!?!?!?
+//    let request = NSMutableURLRequest(URL: NSURL(string: finalURL)!)
+//    
+//    request.setValue(token, forHTTPHeaderField: "Authorization");
+    let MyKeychainWrapper = KeychainWrapper()
+    let createLoginButtonTag = 0
+    let loginButtonTag = 1
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +29,29 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func requestInstagramAccess() {
+        let authURL = NSURL(string: "https://api.instagram.com/oauth/authorize/?client_id=\(CLIENT-ID)&redirect_uri=\(REDIRECT-URI)&response_type=code")
+        UIApplication.sharedApplication().openURL(authURL!)
+    }
+    
+    func exchangeCodeInURL(codeURL : NSURL) {
+        if let code = codeURL.query {
+            let request = NSMutableURLRequest(URL: NSURL(string: "https://github.com/login/oauth/access_token?\(code)&client_id=\(kClientId)&client_secret=\(kClientSecret)")!)
+            request.HTTPMethod = "POST"
+            request.setValue("application/json", forHTTPHeaderField: "Accept")
+            NSURLSession.sharedSession().dataTaskWithRequest(request, completionHandler: { (data, response, error) -> Void in
+                if let httpResponse = response as? NSHTTPURLResponse {
+                    var jsonError: NSError?
+                    if let rootObject = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: &jsonError) as? [String : AnyObject], token = rootObject["access_token"] as? String {
+                        //Save and handle the token
+                    }
+                }
+            }).resume()
+        }
+    }
+    
+    
 
 }
 
